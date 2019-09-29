9b6d29f836aefb7a98424e18d397ec3ac02a16d9
Arief Darmawan <arief@eaciit.com>
2019-09-29 23:39:03 +0700

    add logger

--- server.go
+++ server.go
@@ -13,13 +13,18 @@ type Server struct {
 	log      *toolkit.LogEngine
 }
 
+func (s *Server) SetLogger(logger *toolkit.LogEngine) *Server {
+	s.log = logger
+	return s
+}
+
 func (s *Server) Start(host string) error {
 	if s.log == nil {
 		s.log = toolkit.NewLogEngine(true, false, "", "", "")
 	}
 
 	r := rpc.NewServer()
-	p := new(RpcProxy)
+	p := new(RpcProxy).setLogger(s.log)
 	RegisterRpcObjectToProxy(p, newKvdb())
 	if err := r.Register(p); err != nil {
 		return fmt.Errorf("start server fail. %s", err.Error())
