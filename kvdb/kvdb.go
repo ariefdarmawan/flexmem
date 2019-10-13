b5188413c692a8775f0fd06aa4de90e12602ed90
Arief Darmawan <arief@eaciit.com>
2019-10-13 11:14:55 +0700

    locking on kvdb

--- kvdb/kvdb.go
+++ kvdb/kvdb.go
@@ -2,18 +2,25 @@ package kvdb
 
 import (
 	"fmt"
+	"sync"
 	"time"
 
+	"github.com/ariefdarmawan/flexmem/storage"
 	"github.com/eaciit/toolkit"
 )
 
 type KvDB struct {
 	start time.Time
+
+	lock     *sync.RWMutex
+	storages map[string]*storage.Storage
 }
 
 func NewKvDB() *KvDB {
 	k := new(KvDB)
 	k.start = time.Now()
+	k.lock = new(sync.RWMutex)
+	k.storages = map[string]*storage.Storage{}
 	return k
 }
 
@@ -45,5 +52,25 @@ func (k *KvDB) Save(name string, key string, data interface{}) *SaveResult {
 	if newData {
 		r.Key = key
 	}
+
+	var (
+		found bool
+		list  *storage.Storage
+	)
+	k.lock.RLock()
+	list, found = k.storages[name]
+	k.lock.RUnlock()
+
+	if !found {
+		k.lock.Lock()
+		list = storage.NewStorage(name)
+		k.storages[name] = list
+		k.lock.Unlock()
+	}
+
+	if err := list.Save(key, data); err != nil {
+		r.Error = err
+	}
+
 	return r
 }
